5f3b49a501e27d2eb93f28d6f9ff2b13988d110b
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-09-30 19:55:09 -0300

    fix: modal name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div class="box"
-        on:click={() => teste(card.id)}
+        on:click={() => openEditCardModal(card.id)}
       > 
         <Card {card}/>
       </div>
